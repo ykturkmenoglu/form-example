c25722a4884d783fa52aaaf2831b515965c8ded4
Yalçın Kaan Türkmenoğlu <ykturkmenoglu@gmail.com>
2024-05-29 08:09:50 +0300

    Add files via upload

---
+++ form.js
@@ -0,0 +1,35 @@
+let userFormDOM= document.querySelector("#userForm")
+userFormDOM.addEventListener("submit", formHandler)
+const alertDOM=document.querySelector("#alert")
+
+const alertFunction = (title, message, className="warning") => `<div class="alert alert-${className} alert-dismissible fade show" role="alert">
+<strong>${title}</strong>${message}
+<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+  <span aria-hidden="true">&times;</span>
+</button>
+</div>`
+
+function formHandler(event){
+    event.preventDefault()
+    const USER_NAME= document.querySelector("#username")
+    const SCORE= document.querySelector("#score")
+    if(USER_NAME.value && SCORE.value){
+        addItem(USER_NAME.value, SCORE.value) // gönderdikten sonra sıfırladık
+        USER_NAME.value=""
+        SCORE.value=""
+        alertDOM.innerHTML=alertFunction("Başlık Bilgisi ", "Bilgi girişi başarılı", "success")
+    }
+    else{
+        alertDOM.innerHTML=alertFunction("Başlık Bilgisi ", "Eksik bilgi girdiniz.", "danger")
+    }
+    
+}
+
+let userListDOM=document.querySelector("#userList")
+const addItem = (userName, score) => {
+    let liDOM=document.createElement("li")
+    liDOM.innerHTML=`${userName}
+    <span class="badge badge-primary badge-pill">${score}</span>`
+    liDOM.classList.add("list-group-item", "d-flex", "justify-content-between", "align-items-center")
+    userListDOM.append(liDOM)
+}
